07b7890cceff8d298270f8a24a18ea1d58f8bb43
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2024-03-21 12:15:29 +0545

    add workflow section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import FeatureSection from "./components/FeatureSection";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import Workflow from "./components/Workflow";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
         <FeatureSection />
+        <Workflow />
       </div>
     </>
   );
